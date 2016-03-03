504a86a65719a6581bb633eb191a53396cd82802
lulier <921475341@qq.com>
2016-03-03 21:26:44 +0800

    regular commit

--- GengNiuEnglish/PracticeViewController.m
+++ GengNiuEnglish/PracticeViewController.m
@@ -10,7 +10,6 @@
 
 @interface PracticeViewController ()
 {
-    NSMutableData *_audioBuffer;
     STKAudioPlayer *audioPlayer;
     NSInteger endTime;
     NSTimer *timer;
@@ -72,14 +71,6 @@ static NSString* cellIdentifierLyric=@"LyricViewCell";
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         [self initRecorderSettings];
     });
-}
-- (void) availableBuffer:(id)sender {
-    NSDictionary *userInfo = (NSDictionary *)[sender userInfo];
-    NSData *buffer = userInfo[@"Buffer"];
-    if(_audioBuffer==nil){
-        _audioBuffer=[[NSMutableData alloc]init];
-    }
-    [_audioBuffer appendData:buffer];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -210,7 +201,6 @@ heightForRowAtIndexPath:(NSIndexPath *)indexPath
 //当前的heightlight从一个cell转到另一个cell的时候需要停止前一个cell的所有的录音，播放，识别
 -(void)initRecorder:(NSInteger)index words:(NSArray *)words
 {
-    _audioBuffer=nil;
     if (recognitionResult!=nil)
     {
         recognitionResult=nil;
@@ -218,6 +208,7 @@ heightForRowAtIndexPath:(NSIndexPath *)indexPath
     recognitionResult=[[NSMutableSet alloc]init];
     NSError *error;
     NSURL *soundFileURL=[NSURL URLWithString:[CommonMethod getPath:[NSString stringWithFormat:@"sound%ld.wav",index]]];
+    NSLog(@"log for path:%@",soundFileURL.absoluteString);
     audioRecorder = [[AVAudioRecorder alloc]
                      initWithURL:soundFileURL
                      settings:recordSettings
@@ -237,6 +228,7 @@ heightForRowAtIndexPath:(NSIndexPath *)indexPath
 -(void)stopRecorder:(NSArray*)words index:(NSInteger)index
 {
 //    [[OEPocketsphinxController sharedInstance] suspendRecognition];
+    [audioRecorder stop];
     [self runRecognition:index];
     
 //    [self mergeWavHeaderData:_audioBuffer index:index];
@@ -376,77 +368,6 @@ heightForRowAtIndexPath:(NSIndexPath *)indexPath
     NSLog(@"Pocketsphinx has suspended recognition.");
 }
 
-//添加wav头
--(NSMutableData *)mergeWavHeaderData:(NSMutableData *)data index:(NSInteger)index
-{
-    
-    long totalAudioLen = 0;
-    long totalDataLen = 0;
-    long longSampleRate = 16000;//11025.0
-    int channels = 1;
-    long byteRate = 16 * 16000 * channels/8;
-    
-    totalAudioLen=[data length];
-    totalDataLen=totalAudioLen+44;
-    
-    
-    Byte *header=(Byte *)malloc(44);
-    header[0] = 'R'; // RIFF/WAVE header
-    header[1] = 'I';
-    header[2] = 'F';
-    header[3] = 'F';
-    header[4] = (Byte) (totalDataLen & 0xff);
-    header[5] = (Byte) ((totalDataLen >> 8) & 0xff);
-    header[6] = (Byte) ((totalDataLen >> 16) & 0xff);
-    header[7] = (Byte) ((totalDataLen >> 24) & 0xff);
-    header[8] = 'W';
-    header[9] = 'A';
-    header[10] = 'V';
-    header[11] = 'E';
-    header[12] = 'f'; // 'fmt ' chunk
-    header[13] = 'm';
-    header[14] = 't';
-    header[15] = ' ';
-    header[16] = 16; // 4 bytes: size of 'fmt ' chunk
-    header[17] = 0;
-    header[18] = 0;
-    header[19] = 0;
-    header[20] = 1; // format = 1
-    header[21] = 0;
-    header[22] = (Byte) channels;
-    header[23] = 0;
-    header[24] = (Byte) (longSampleRate & 0xff);
-    header[25] = (Byte) ((longSampleRate >> 8) & 0xff);
-    header[26] = (Byte) ((longSampleRate >> 16) & 0xff);
-    header[27] = (Byte) ((longSampleRate >> 24) & 0xff);
-    header[28] = (Byte) (byteRate & 0xff);
-    header[29] = (Byte) ((byteRate >> 8) & 0xff);
-    header[30] = (Byte) ((byteRate >> 16) & 0xff);
-    header[31] = (Byte) ((byteRate >> 24) & 0xff);
-    header[32] = (Byte) (2 * 8 / 8); // block align
-    header[33] = 0;
-    header[34] = 16; // bits per sample
-    header[35] = 0;
-    header[36] = 'd';
-    header[37] = 'a';
-    header[38] = 't';
-    header[39] = 'a';
-    header[40] = (Byte) (totalAudioLen & 0xff);
-    header[41] = (Byte) ((totalAudioLen >> 8) & 0xff);
-    header[42] = (Byte) ((totalAudioLen >> 16) & 0xff);
-    header[43] = (Byte) ((totalAudioLen >> 24) & 0xff);
-    
-    NSData *headerData=[NSData dataWithBytes:header length:44];
-    
-    NSMutableData *soundData=[[NSMutableData alloc]init];
-    [soundData appendData:[headerData subdataWithRange:NSMakeRange(0, 44)]];
-    [soundData appendData:data];
-    
-    [soundData writeToFile:[CommonMethod getPath:[NSString stringWithFormat:@"sound%ld.wav",index]] atomically:YES];
-    NSLog(@"log for path:%@",[CommonMethod getPath:[NSString stringWithFormat:@"sound%ld.wav",index]]);
-    return soundData;
-    
-}
 /*
 #pragma mark - Navigation
 
