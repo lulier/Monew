6a66622401b5551c863ec3a29c5fb82d082af4ab
lulier <921475341@qq.com>
2016-04-25 15:25:56 +0800

    添加图片和设置界面

---
+++ GengNiuEnglish/SettingViewController.h
@@ -0,0 +1,14 @@
+//
+//  SettingViewController.h
+//  GengNiuEnglish
+//
+//  Created by luzegeng on 16/4/25.
+//  Copyright © 2016年 luzegeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *settingView;
+
+@end
---
+++ GengNiuEnglish/SettingViewController.m
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.m
+//  GengNiuEnglish
+//
+//  Created by luzegeng on 16/4/25.
+//  Copyright © 2016年 luzegeng. All rights reserved.
+//
+
+#import "SettingViewController.h"
+
+@implementation SettingViewController
+
+@end
